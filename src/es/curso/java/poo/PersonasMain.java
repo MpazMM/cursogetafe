79cf4da1c41d3b527d59f2828aead4fa4116d84d
Mpaz <email@email.es>
2023-09-20 19:49:31 +0200

    Programación objetos

--- src/es/curso/java/poo/PersonasMain.java
+++ src/es/curso/java/poo/PersonasMain.java
@@ -11,11 +11,19 @@ public class PersonasMain {
 		//System.out.println(p1);
 		System.out.println(p1.nombre);
 		System.out.println(p1.edad);
-		
-		
 		p1.nombre = "Eva";
 		System.out.println(p1.nombre);
 		
+		Persona p2 = new Persona();
+		//Para no perder la información del objeto Persona1
+		//Metemos la información en la variable p3
+		Persona p3 = p1;
+		p1 = p2;
+		p2.nombre = "María";
+		
+		System.out.println(p3.nombre);
+		
+		p1.correr();
 		
 		
 		
